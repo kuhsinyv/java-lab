c9147fca601211aa23b8cf3855462188a5f028d8
kuhsinyv <XeuphoriaGu@163.com>
2021-03-09 14:14:19 +0800

    :tada:

---
+++ src/cn/edu/ncu/guxinyu/chapter01/HelloWorld.java
@@ -0,0 +1,15 @@
+package cn.edu.ncu.guxinyu.chapter01;
+
+/**
+ * 第一个 Java 程序
+ * @author Gu Xinyu
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * @param args command line args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
